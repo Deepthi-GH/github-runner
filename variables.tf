eb5cce776a30468cc9ba049b00ddba338a04b944
Deepthi Kattika <deepthi.kattika@gmail.com>
2026-01-28 11:26:33 +0800

    github actions

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "deepthi.cloud"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z10084241A7O0LJXGH49U"
+  description = "description"
+}
